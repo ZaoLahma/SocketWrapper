1a7508880b2389fcc6b16bad532aa42d16b5ffdb
Janne <thyberg.jan@gmail.com>
2016-08-21 19:33:07 +0200

    Removed unnecessary printouts

--- src/socket_api.cpp
+++ src/socket_api.cpp
@@ -148,6 +148,7 @@ SocketBuf SocketAPI::receiveData(int fileDescriptor, int noOfBytes, bool expectA
 
 		noOfBytes -= numbytes;
 		retVal.dataSize += numbytes;
+
 		if(false == expectAllBytes && numbytes == 0) {
 			break;
 		}
